c43ad90b32979464c0d5460edf425f60c060ed72
Jason Hejna <jason.hejna@gmail.com>
2015-09-05 15:18:45 -0400

    ejs bootstrap integration

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/jquery.min.map', function(req, res, next) {
+  //res.sendFile(path.join(__dirname, '../../public/bootstrap/js', 'jquery.min.map'));
+  res.download('public/bootstrap/js/jquery.min.map');
+});
+
+
+
 module.exports = router;
