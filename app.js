9ecb693b23f7be27695c5e389615efb08681726a
Jason Hejna <jason.hejna@gmail.com>
2015-09-16 23:09:39 -0400

    start refactor

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //use public as our fallback root directory. Make sure nginx is setup to serve public in prod!
-app.use(express.static('public'));
+app.use(express.static('app'));
 
 app.use('/', routes);
 app.use('/users', users);
